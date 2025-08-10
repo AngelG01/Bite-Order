3aed54a9a6576e149cd12c051c336b576a315035
Angel <angelgyurov37383@gmail.com>
2025-08-10 23:34:15 +0300

    testing

--- bite_order/settings.py
+++ bite_order/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "bite_order.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
